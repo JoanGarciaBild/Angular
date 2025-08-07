9df870f9311488d616c556875f528d99f6713973
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-08-06 23:05:44 -0700

    TypeScript Destructuración de Objetos

--- 01-typescript-intro/src/topics/04-homework-types.ts
+++ 01-typescript-intro/src/topics/04-homework-types.ts
@@ -9,9 +9,9 @@ interface SuperHero {
 }
 
 interface Address {
-    calle: string;
-    pais: string;
-    ciudad: string;
+    street: string;
+    country: string;
+    city: string;
 }
 
 
@@ -19,12 +19,12 @@ const superHeroe: SuperHero = {
     name: 'Spiderman',
     age: 30,
     address: {
-        calle: 'Main St',
-        pais: 'USA',
-        ciudad: 'NY'
+        street: 'Main St',
+        country: 'USA',
+        city: 'NY'
     },
     showAddress() {
-        return this.name + ', ' + this.address.ciudad + ', ' + this.address.pais;
+        return this.name + ', ' + this.address.city + ', ' + this.address.country;
     }
 }
 
